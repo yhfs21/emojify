81ce1cd93e1f52ca1a5e35ba2ea55af2bdc4c463
y-fuse <y-fuse@wwwave.jp>
2024-09-22 10:36:46 +0900

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.27.3
      project name = emojify
      framework = next
      framework cli = create-next-app@14.2.5
      package manager = npm@9.4.0
      wrangler = wrangler@3.78.6
      git = 2.37.1 (Apple Git-137.1)

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextRequest } from 'next/server'
+import { getRequestContext } from '@cloudflare/next-on-pages'
 
-type Data = {
-  name: string;
-};
+export const config = {
+  runtime: 'edge',
+}
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextRequest) {
+  let responseText = 'Hello World'
+
+  // In the edge runtime you can use Bindings that are available in your application
+  // (for more details see:
+  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+  //    - https://developers.cloudflare.com/pages/functions/bindings/
+  // )
+  //
+  // KV Example:
+  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
+  // await myKv.put('suffix', ' from a KV store!')
+  // const suffix = await myKv.get('suffix')
+  // responseText += suffix
+
+  return new Response(responseText)
 }
